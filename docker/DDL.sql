9f0fe6e1957c917985d8585c115df2a9a7c190e4
KKKZOZ <70520782+KKKZOZ@users.noreply.github.com>
2022-07-11 14:16:30 +0800

    first commit

---
+++ docker/DDL.sql
@@ -0,0 +1,12 @@
+create table platform_user(
+    id int,
+    username varchar(255),
+    password varchar(255),
+    email varchar(255),
+    role varchar(255),
+
+);
+
+create table group(
+    group_id bigserial,
+)
