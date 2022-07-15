17cd2aae46748457ad20b8ed0a8a870fabdf5ad3
KKKZOZ <70520782+KKKZOZ@users.noreply.github.com>
2022-07-15 22:13:51 +0800

    update plan service

--- plan-service/src/main/java/org/jff/cloud/PlanService.java
+++ plan-service/src/main/java/org/jff/cloud/PlanService.java
@@ -2,6 +2,7 @@ package org.jff.cloud;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jff.cloud.dto.TeachingPlanDTO;
 import org.jff.cloud.entity.TeachingDay;
 import org.jff.cloud.entity.TeachingPlan;
 import org.jff.cloud.global.ResponseVO;
@@ -9,6 +10,9 @@ import org.jff.cloud.global.ResultCode;
 import org.jff.cloud.mapper.TeachingDayMapper;
 import org.jff.cloud.mapper.TeachingPlanMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -21,7 +25,58 @@ public class PlanService {
 
 
     public ResponseVO addPlan(TeachingPlan teachingPlan) {
+        log.info("addPlan: {}", teachingPlan);
         teachingPlanMapper.insert(teachingPlan);
         return new ResponseVO(ResultCode.SUCCESS, "添加教学计划成功");
+    }
+
+    public ResponseVO addDay(TeachingDay teachingDay) {
+        log.info("addDay: {}", teachingDay);
+        teachingDayMapper.insert(teachingDay);
+        return new ResponseVO(ResultCode.SUCCESS, "添加教学天成功");
+    }
+
+    public ResponseVO deletePlan(Long teachingPlanId) {
+        log.info("deletePlan: {}", teachingPlanId);
+        teachingPlanMapper.deleteById(teachingPlanId);
+        return new ResponseVO(ResultCode.SUCCESS, "删除教学计划成功");
+    }
+
+    public List<TeachingPlanDTO> getAllPlans() {
+
+        List<TeachingPlanDTO> list = new ArrayList<>();
+
+        List<TeachingPlan> teachingPlans = teachingPlanMapper.selectList(null);
+
+        for (TeachingPlan teachingPlan : teachingPlans) {
+            TeachingPlanDTO teachingPlanDTO = TeachingPlanDTO.builder()
+                    .teachingPlanId(teachingPlan.getId())
+                    .name(teachingPlan.getName())
+                    .description(teachingPlan.getDescription())
+                    .startDate(teachingPlan.getStartDate())
+                    .endDate(teachingPlan.getEndDate())
+                    .build();
+            list.add(teachingPlanDTO);
+        }
+
+        return list;
+    }
+
+    public List<TeachingDay> getAllDays(Long teachingPlanId) {
+        List<TeachingDay> teachingDays = teachingDayMapper.selectList(null);
+        log.info("getAllDays: {}", teachingDays);
+        return teachingDays;
+    }
+
+    public ResponseVO updateDay(TeachingDay teachingDay) {
+        log.info("updateDay: {}", teachingDay);
+        teachingDayMapper.updateById(teachingDay);
+        return new ResponseVO(ResultCode.SUCCESS, "修改教学天成功");
+    }
+
+    public ResponseVO updatePlan(TeachingPlan teachingPlan) {
+        log.info("updatePlan: {}", teachingPlan);
+        teachingPlanMapper.updateById(teachingPlan);
+        return new ResponseVO(ResultCode.SUCCESS, "修改教学计划成功");
     }
 }
---
+++ plan-service/src/main/java/org/jff/cloud/dto/TeachingPlanDTO.java
@@ -0,0 +1,25 @@
+package org.jff.cloud.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeachingPlanDTO {
+
+    private Long teachingPlanId;
+
+    private String name;
+
+    private String description;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+}
